78e61ebb16e0de855111ac52e1217c4e7f26533d
Olyrascode <120477523+Olyrascode@users.noreply.github.com>
2024-01-05 18:40:15 +0100

    Delete src directory

--- src/components/Banner.jsx
+++
@@ -1,9 +0,0 @@
-import BannerHome from './assets/images/bannerHome.png';
-function Banner() {
-    return (
-        <div className='banner'>
-            <img className='banner_img' src={BannerHome} alt="banniere image" />
-            <h1 className="banner_tilte">Chez vous, partout et ailleurs</h1>
-        </div>
-    )
-}export default Banner
--- src/components/Banner2.jsx
+++
@@ -1,8 +0,0 @@
-import BannerPropos from './assets/images/bannerPropos.png';
-function Banner2() {
-    return (
-        <div className='banner banner2'>
-            <img className='banner_img' src={BannerPropos} alt="banniere image" />
-        </div>
-    )
-}export default Banner2
